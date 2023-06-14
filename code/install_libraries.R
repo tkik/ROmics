08fc96c22410364ded3e2f4f02c6fb91b5e0a332
tkik <rtoth81@gmail.com>
2023-06-14 15:45:45 +0200

    glimma, install file and count data

--- code/install_libraries.R
+++ code/install_libraries.R
@@ -3,3 +3,29 @@ install.packages(c("workflowr", "dplyr", "ggplot2", "rmarkdown", "DT"))
 if (!require("BiocManager", quietly = TRUE))
   install.packages("BiocManager")
 BiocManager::install(c("GenomicRanges", "rtracklayer", "airway"))
+
+
+install.packages(c("ggfortify", "ggpubr", "ggplot2", "ggsci", "corrplot", "sva", "pheatmap"))
+
+if (!require("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+BiocManager::install(c( "GEOquery", "DESeq2", "Glimma", "AnnotationDbi", "org.Hs.eg.db",
+                        "clusterProfiler", "DOSE", "enrichplot", "topGO", "GeneTonic" ))
+
+
+library(GEOquery)
+library(DESeq2)
+library(ggfortify)
+library(ggpubr)
+library(ggsci)
+library(Glimma)
+library(corrplot)
+library(sva)
+library("AnnotationDbi")
+library("org.Hs.eg.db")
+library("clusterProfiler")
+library(pheatmap)
+library(DOSE)
+library(enrichplot)
+library(topGO)
+library(GeneTonic)
